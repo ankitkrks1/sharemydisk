723ae08422dddf265e513c503be9fb886ae7f9be
Ankit Kumar Kashyap <ankitkrks@ankitkrks.local>
2024-04-03 21:39:47 +0530

    package.json eddited for publishing over npm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
+
 var serveIndex = require('serve-index') // this package is used to server folder like filemanager
 const app = express()
 const getPort = require('./src/getPort')
@@ -8,8 +8,6 @@ const getDirectory = require('./src/getDirectory')
 const port = getPort(process.argv)
 
 const location = getDirectory(process.argv)
-
-app.use(cors())
 
 //Servers the Ftp type files stores in Files Folder
 app.use('/', express.static(location), serveIndex(location))
